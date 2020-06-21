a26e373e95c9040ceb9a2a7845609744f237b7d2
mainalisriya <55081934+mainalisriya@users.noreply.github.com>
2020-06-21 10:56:19 -0700

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,15 @@
+const keys=document.querySelectorAll('.key')
+
+keys.forEach(key => {
+    key.addEventListener('click',()=>playNote(key))
+})
+function playNote(key){
+    const note=document.getElementById(key.dataset.note)
+    note.currentTime=0
+    note.play()
+
+    key.classList.add('active')
+    note.addEventListener('ended',()=>{
+        key.classList.remove('active')
+    })
+}
